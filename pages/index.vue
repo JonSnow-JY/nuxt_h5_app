4de3d6d2570d9c2f49108902869df61e73c0d35a
JonSnow <1571298579@qq.com>
2019-12-23 14:38:58 +0800

    写了一首诗

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <div class="">
-    几哈的甲方哈卡水电费哈萨克打飞机安徽的付款后撒旦教发哈萨克积分兑换
+    每个人都有一个死角，自己走不出来，别人也走不进去；我把最深沉的秘密藏在这里，你不懂我，我不怪你。
     <van-button type="default">默认按钮</van-button>
   </div>
 </template>
